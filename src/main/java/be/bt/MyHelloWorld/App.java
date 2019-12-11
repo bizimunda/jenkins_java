86426c0cea5ba23f4fa2649a192fdedf5ebd8044
Hamid <hamid@gmail.com>
2019-12-11 16:34:31 +0100

    initial commit

---
+++ src/main/java/be/bt/MyHelloWorld/App.java
@@ -0,0 +1,18 @@
+package be.bt.MyHelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	for (int i = 1; i < 6; i++) {
+    		
+    		System.out.println( "Itération: "+ i + " ==> Welcome to Java by Maven" );
+			
+		}
+        
+    }
+}
